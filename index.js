2b6a8a180e9df99f5a5cb1701f125b5885a1353a
nadyaivanisheva@gmail.com <nadyaivanisheva@gmail.com>
2018-03-10 21:29:00 +0000

    Express usage

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
+const express = require('express');
+const app = express();
 
 var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(port, () => {
+  console.log('We are live on ');
+});
