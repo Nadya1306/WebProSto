0762ec95147b81bf70bebf3b2fe37c5dddd03d6f
nadyaivanisheva@gmail.com <nadyaivanisheva@gmail.com>
2018-03-10 21:34:09 +0000

    get method is added

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 var port = process.env.PORT || 1337;
+
 app.listen(port, () => {
   console.log('We are live on ');
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
