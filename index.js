54046c4253c8f35ea11e68b9e0764dd87baa7df3
nadyaivanisheva@gmail.com <nadyaivanisheva@gmail.com>
2018-03-25 15:57:06 +0100

    commit for list.json

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 var port = process.env.PORT || 1337;
 
 app.listen(port, () => {
-  console.log('We are live on ');
+  console.log('We are live on - ' + port);
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  const content = fs.readFileSync('list.json');
+  const data = JSON.parse(content);
+  res.send(data);
 });
